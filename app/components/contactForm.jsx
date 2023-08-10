17e54121c397e15e8245fed4c2b7078718b19ce2
demonducky <cuti1mat@gmail.com>
2023-08-10 21:02:15 +0700

    change toast position

--- app/components/contactForm.jsx
+++ app/components/contactForm.jsx
@@ -18,6 +18,16 @@ export default function ContactForm() {
 
     const [form, setForm] = useState({})
 
+    // const inputs = [
+    //     {
+    //         type: "text",
+    //         label: "Your Name",
+    //         name: "name",
+    //         placeholder: "Eg: Michael Schofield"
+    //     }
+    // ]
+    //
+
     async function handleSubmit(e) {
         e.preventDefault()
         const response = fetch("https://announce.fly.dev/add-job", {
@@ -33,14 +43,24 @@ export default function ContactForm() {
             {
                 pending: 'Sending...',
                 success: 'Success, I will contact to you ASAP',
-                error: 'Failed, please contact me manually. Sorry for inconvenient'
+                error: 'Failed, please contact me manually. Sorry for the inconvenient'
             }
         )
+
+        if ((await response).ok) setForm({})
+    }
+
+    function changeHandler(e) {
+        if (e.target.value === '') {
+            setForm({...form, [e.target.name]: "null"})
+        } else {
+            setForm({...form, [e.target.name]: e.target.value})
+        }
     }
 
     return (
         <>
-            <ToastContainer/>
+            <ToastContainer position={"bottom-right"}/>
             <Dialog>
                 <DialogTrigger asChild><Button variant={"destructive"} className={"text-lg"}>Let me
                     contact
@@ -55,27 +75,32 @@ export default function ContactForm() {
                     <form onSubmit={handleSubmit}>
                         <div className={"text-left flex flex-col gap-y-6 mb-6"}>
                             <div><Label htmlFor={"name"}>Your Name</Label>
-                                <Input type={"text"} id={"name"} name={"name"} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+                                <Input type={"text"} id={"name"} name={"name"}
+                                       onChange={(e) => changeHandler(e)}
                                        placeholder={"Eg: Michael Schofield"}></Input>
                             </div>
                             <div>
                                 <Label htmlFor={"email"}>Email</Label>
-                                <Input type={"email"} id={"email"} name={"email"} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+                                <Input type={"email"} id={"email"} name={"email"}
+                                       onChange={(e) => changeHandler(e)}
                                        placeholder={"Eg: example@gmail.com"}></Input>
                             </div>
                             <div>
                                 <Label htmlFor={"contact"}>Other contact</Label>
-                                <Input type={"text"} id={"contact"} name={"contact"} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+                                <Input type={"text"} id={"contact"} name={"contact"}
+                                       onChange={(e) => changeHandler(e)}
                                        placeholder={"Eg: discord: .demonducky, instagram: tuananh.lg"}></Input>
                             </div>
                             <div>
                                 <Label htmlFor={"service"}>What service do you need?</Label>
-                                <Input type={"text"} id={"service"} name={"service"} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+                                <Input type={"text"} id={"service"} name={"service"}
+                                       onChange={(e) => changeHandler(e)}
                                        placeholder={"Eg: E-commerce website, spigot plugin,..."}></Input>
                             </div>
                             <div>
                                 <Label htmlFor={"information"}>Additional information</Label>
-                                <Textarea type={"text"} id={"information"} name={"information"} onChange={(e) => setForm({...form, [e.target.name]: e.target.value})}
+                                <Textarea type={"text"} id={"information"} name={"information"}
+                                          onChange={(e) => changeHandler(e)}
                                           placeholder={"Write anything you feel I need to know (not required)"}></Textarea>
                             </div>
                         </div>
